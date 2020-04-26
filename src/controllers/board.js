0eb93d40fd4ac71e26f36f84110c6593a20f959a
DALyskov <DALyskov@gmail.com>
2020-04-26 13:51:21 +0300

    Переделал метод _onDataChange

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -94,7 +94,7 @@ class BoardController {
     });
   }
 
-  _onDataChange(taskController, oldData, newData) {
+  _onDataChange(oldData, newData) {
     const index = this._tasks.findIndex((it) => it === oldData);
 
     if (index === -1) {
@@ -103,7 +103,7 @@ class BoardController {
 
     this._tasks = [].concat(this._tasks.slice(0, index), newData, this._tasks.slice(index + 1));
 
-    taskController.render(this._tasks[index]);
+    this._showedTaskControllers[index].render(this._tasks[index]);
   }
 
   _onSortTypeChange(sortType) {
